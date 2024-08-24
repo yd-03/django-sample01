49a852c14ff3f84aeef271529ddadb505cbec48b
yd-03 <3nu4ydye57@gmail.com>
2024-08-24 15:16:20 +0900

    fix: URL patternの修正

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("polls.urls")),  # include()は他のURLconfを参照することができる
+    path(
+        "polls/", include("polls.urls")
+    ),  # include()は他のURLconfを参照することができる
     path("admin/", admin.site.urls),
 ]
