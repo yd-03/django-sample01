fbbca4720dc63ca17a608dad003f6cbb8542436f
yd-03 <3nu4ydye57@gmail.com>
2024-08-25 13:45:02 +0900

    feat: resultsテンプレートの追加

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
 from django.shortcuts import render, get_object_or_404
@@ -18,8 +18,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
